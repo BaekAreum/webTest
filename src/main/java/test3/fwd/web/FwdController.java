6dbfec5d58803e34c0d267f1477325af14914f49
BaekAreum <20191775@edu.hanbat.ac.kr>
2022-09-02 22:12:13 +0900

    웹페이지 연결(jsp)

--- src/main/java/test3/fwd/web/FwdController.java
+++ src/main/java/test3/fwd/web/FwdController.java
@@ -2,6 +2,7 @@ package test3.fwd.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,7 +12,24 @@ import test3.fwd.service.FwdVO;
 public class FwdController {
 	@RequestMapping(value = "/reqUrl.do")
 	public String selectReqUrl(@ModelAttribute("searchVO") FwdVO searchVO, ModelMap model) throws Exception{
-		return "redirect:http://www.naver.com";
+		return "redirect:https://www.naver.com/";
+	}
+	
+	@GetMapping("/main.do")
+	public String mainPage() {
+		return "main";
+	}
+	@GetMapping("/detail.do")
+	public String detailPage() {
+		return "detail";
+	}
+	@GetMapping("/display.do")
+	public String displayPage() {
+		return "display";
+	}
+	@GetMapping("/risk.do")
+	public String riskPage() {
+		return "risk";
 	}
 
 
